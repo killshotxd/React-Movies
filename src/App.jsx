6a8478e51fe63c620b18600da0dad8971c309be1
killshotxd <hassanansari211@gmail.com>
2022-09-22 20:31:37 +0530

    Navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
