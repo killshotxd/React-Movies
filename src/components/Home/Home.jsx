0bea1604e47fef31b87c83923f529a56e57b2506
killshotxd <hassanansari211@gmail.com>
2022-09-23 10:08:26 +0530

    Paginate component ready

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Paginate from "../Paginate/Paginate";
 import styles from "./Home.module.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Paginate>
+        <div style={{ height: "500px", width: "100%", background: "pink" }} />
+        <div style={{ height: "500px", width: "100%", background: "orange" }} />
+        <div style={{ height: "500px", width: "100%", background: "tomato" }} />
+      </Paginate>
+    </div>
+  );
 };
 
 export default Home;
