63d368e591584e7a838d04d03ac0d246ee746bee
killshotxd <hassanansari211@gmail.com>
2022-09-23 10:20:30 +0530

    Fixed 2 times API req fetch

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Paginate from "../Paginate/Paginate";
 import styles from "./Home.module.css";
 import { getPopularMovies } from "../../api/movies";
 const Home = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isMoreMoviesLoading, setIsMoreMoviesLoading] = useState(false);
+  const [isNearEnd, setIsNearEnd] = useState(false);
+
   const fetchPopularMovies = () => {
     getPopularMovies().then((res) => {
       if (!res) return;
